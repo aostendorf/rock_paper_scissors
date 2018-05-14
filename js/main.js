a31a5779d7951282b589597b7e8ec8e56a22c171
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-14 17:22:21 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+
+
+
+/* function game(pick1, pick2)
+  console.log(pick1 + pick2);
+  switch(pick1, pick2) {
+    case 'pick1 === "Scissors" && pick2 === "Paper"':
+    console.log 'You push select again';
+    break;
+    case 'pick1 === "Paper" && pick2 === "Rock"':
+    console.log 'Paper wraps rock. Paper wins!';
+    break;
+    case 'pick1 === pick2':
+    console.log = 'You push, select again';
+}
+
+/* rock = rock
+paper = paper
+scissors = scissors
+    push then select again
+
+paper === rock || rock === paper
+    paper wraps rock - paper wins
+    
+scissors === paper || paper ===scissors
+    scissors cuts paper - scissors win
+    
+rock === scissors || scissors === rock
+    rock crushes scissors - rock wins
+    */
